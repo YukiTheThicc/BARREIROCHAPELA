c775093b7f4d0cf88f268945daeb9ea9556eda33
Santi-PC <santiago191098@gmail.com>
2021-01-17 23:10:08 +0100

    V0.1.1 Por hacer informes de clientes, productos: facturas enteras y backup

--- conexion.py
+++ conexion.py
@@ -168,27 +168,30 @@ class Conexion():
     def actualizar_tabla_pro():
         index = 0
         query = QtSql.QSqlQuery()
-        query.prepare('select codigo, nombre, precio_unidad from articulos')
+        query.prepare('select codigo, nombre, precio_unidad, stock from articulos')
         if query.exec_():
             while query.next():
                 codigo = query.value(0)
                 nombre = query.value(1)
                 precio = query.value(2)
+                stock = query.value(3)
                 var.ui.tbl_pro_tabla.setRowCount(index + 1)  # crea la fila y a continuación mete los datos
                 var.ui.tbl_pro_tabla.setItem(index, 0, QtWidgets.QTableWidgetItem(str(codigo)))
                 var.ui.tbl_pro_tabla.setItem(index, 1, QtWidgets.QTableWidgetItem(nombre))
                 var.ui.tbl_pro_tabla.setItem(index, 2, QtWidgets.QTableWidgetItem(str(precio)))
+                var.ui.tbl_pro_tabla.setItem(index, 3, QtWidgets.QTableWidgetItem(str(stock)))
                 index += 1
         else:
             print("Error al actualizar la tabla de productos: ", query.lastError().text())
 
     @staticmethod
-    def alta_producto(producto: [str, float]):
+    def alta_producto(producto: [str, float, int]):
         query = QtSql.QSqlQuery()
-        query.prepare('insert into articulos (nombre, precio_unidad)'
-                      'VALUES (:nombre, :precio_unidad)')
+        query.prepare('insert into articulos (nombre, precio_unidad, stock)'
+                      'VALUES (:nombre, :precio_unidad, :stock)')
         query.bindValue(':nombre', str(producto[0]))
         query.bindValue(':precio_unidad', round(producto[1], 2))
+        query.bindValue(':stock', producto[2])
         if query.exec_():
             print("Inserción de Producto Correcta")
             Conexion.actualizar_tabla_pro()
@@ -210,11 +213,12 @@ class Conexion():
     def modif_producto(codigo, nuevos_datos):
         query = QtSql.QSqlQuery()
         codigo = int(codigo)
-        query.prepare('update articulos set nombre=:nombre, precio_unidad=:precio '
+        query.prepare('update articulos set nombre=:nombre, precio_unidad=:precio, stock=:stock '
                       'where codigo=:codigo')
         query.bindValue(':codigo', int(codigo))
         query.bindValue(':nombre', nuevos_datos[0])
         query.bindValue(':precio', round(nuevos_datos[1], 2))
+        query.bindValue(':stock', nuevos_datos[2])
         if query.exec_():
             print('Producto modificado')
             var.ui.lbl_status.setText('El producto  ' + str(nuevos_datos[0]) + ' ha sido modificado')
--- productos/productos.py
+++ productos/productos.py
@@ -9,8 +9,9 @@ class Productos:
     def alta_producto():
         nombre = var.ui.edit_pro_nombre.text()
         precio = var.ui.dspin_pro_precio.value()
+        stock = var.ui.spin_pro_stock.value()
         if not nombre == '':
-            to_insert = [nombre, precio]
+            to_insert = [nombre, precio, stock]
             conexion.Conexion.alta_producto(to_insert)
         else:
             events.Eventos.aviso("Necesita un nombre")
@@ -20,12 +21,13 @@ class Productos:
         try:
             tupla_elegida = var.ui.tbl_pro_tabla.selectedItems()
             campos_producto = {"nombre": var.ui.edit_pro_nombre, "precio": var.ui.dspin_pro_precio,
-                               "codigo": var.ui.lbl_pro_muestra_codigo}
+                               "codigo": var.ui.lbl_pro_muestra_codigo, "stock": var.ui.spin_pro_stock}
             campos_producto["codigo"].setText(tupla_elegida[0].text())
             campos_producto["nombre"].setText(tupla_elegida[1].text())
             campos_producto["precio"].setValue(float(tupla_elegida[2].text()))
+            campos_producto["stock"].setValue(int(tupla_elegida[3].text()))
         except Exception as error:
-            print('Error en cargarCli: %s ' % str(error))
+            print('Error en sel_producto: %s ' % str(error))
 
     @staticmethod
     def limpiar_campos():
@@ -37,8 +39,9 @@ class Productos:
             var.ui.edit_pro_nombre.setText('')
             var.ui.lbl_pro_muestra_codigo.setText('')
             var.ui.dspin_pro_precio.setValue(0.01)
+            var.ui.edit_pro_stock.setValue(0)
         except Exception as error:
-            print('Error en limpiarCliente: %s ' % str(error))
+            print('Error en limpiar_producto: %s ' % str(error))
 
     @staticmethod
     def baja_producto():
@@ -48,13 +51,13 @@ class Productos:
             conexion.Conexion.actualizar_tabla_pro()
             Productos.limpiar_campos()
         except Exception as error:
-            print('Error en bajaCliente: %s ' % str(error))
+            print('Error en baja_producto: %s ' % str(error))
 
     @staticmethod
     def modif_producto():
         try:
             codigo = var.ui.lbl_pro_muestra_codigo.text()
-            newdata = [var.ui.edit_pro_nombre.text(), var.ui.dspin_pro_precio.value()]
+            newdata = [var.ui.edit_pro_nombre.text(), var.ui.dspin_pro_precio.value(), var.ui.spin_pro_stock.value()]
             conexion.Conexion.modif_producto(codigo, newdata)
             conexion.Conexion.actualizar_tabla_pro()
         except Exception as error:
@@ -67,7 +70,7 @@ class Productos:
             conexion.Conexion.actualizar_tabla_pro()
             print('Recargando...')
         except Exception as error:
-            print('Error en recargar: %s ' % str(error))
+            print('Error en recargar_producto: %s ' % str(error))
 
     @staticmethod
     def buscar():
@@ -78,4 +81,4 @@ class Productos:
             else:
                 print('Se ha intentado buscar por un nombre vacio')
         except Exception as error:
-            print('Error en buscar producto: %s ' % str(error))
+            print('Error en buscar_producto: %s ' % str(error))
